f402402b4f5437027be610a71a6ab25bf56063e7
svetlana <svetlana@Lenovo-PC>
2020-03-25 16:56:39 +0200

    last update - class: AdminController method:users() by anna

---
+++ src/main/java/demo/AdminController.java
@@ -0,0 +1,29 @@
+package demo;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+	//Anna -Admin API -Export all users
+	UserController uc = new UserController();
+	@RequestMapping(path = "/acs/admin/users/{adminDomain}/{adminEmail}",
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public UserBoundary[] users (
+			@PathVariable("adminDomain") String adminDomain,
+			@PathVariable("adminEmail") String adminEmail) {
+		return
+			IntStream.range(0, 5) // Stream of Integer
+			.mapToObj(i->uc.loginValidUser(adminDomain, adminEmail)) // Stream of UserBoundry
+			.collect(Collectors.toList()) // List of UserBoundry
+			.toArray(new UserBoundary[0]); // ComplexMessagBoundary[]
+	}
+
+}
--- src/main/java/demo/UserController.java
+++ src/main/java/demo/UserController.java
@@ -1,6 +1,7 @@
 package demo;
 
-
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,37 +16,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-	// Sapir - User related API - Login valid user 	
-		@RequestMapping(path = "/acs/users/login/{userDomain}/{userEmail}",
-				method = RequestMethod.GET,
-				produces = MediaType.APPLICATION_JSON_VALUE)
-		
-		public UserBoundary loginValidUser (@PathVariable("userDomain") String userDomain , @PathVariable("userEmail") String userEmail) {
-			if (userDomain != null && !userDomain.trim().isEmpty() && userEmail != null && !userEmail.trim().isEmpty()) {
-				//return new UserBoundary();
-				UserBoundary ub = new UserBoundary();
-				ub.setUserId(new UserIdBoundary(userDomain, userEmail));
-				ub.setTypeRole(TypeEnumRole.PLAYER); //???
-				ub.setUsername("Demo User"); //???
-				ub.setAvater(";-)"); //???
-				return ub;
-			}else {
-				throw new NameNotFoundException("Invalid user name/email");
-			}
-		}		
-	//Sapir - User related API - Create a new user
-		@RequestMapping(path = "/acs/users",
-				method = RequestMethod.POST,
-				produces = MediaType.APPLICATION_JSON_VALUE,
-				consumes = MediaType.APPLICATION_JSON_VALUE)
-		public UserBoundary CreateNewUser (
-				@RequestBody NewUserDetailsBoundary userDetails) {
+	// Sapir - User related API - Login valid user
+	@RequestMapping(path = "/acs/users/login/{userDomain}/{userEmail}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+
+	public UserBoundary loginValidUser(@PathVariable("userDomain") String userDomain,
+			@PathVariable("userEmail") String userEmail) {
+		if (userDomain != null && !userDomain.trim().isEmpty() && userEmail != null && !userEmail.trim().isEmpty()) {
+			// return new UserBoundary();
 			UserBoundary ub = new UserBoundary();
-			ub.setUserId(new UserIdBoundary("2020b.demo", userDetails.getEmail())); //???
-			ub.setTypeRole(userDetails.getTypeRole());
-			ub.setUsername(userDetails.getUsername()); 
-			ub.setAvater(userDetails.getAvater()); 
+			ub.setUserId(new UserIdBoundary(userDomain, userEmail));
+			ub.setTypeRole(TypeEnumRole.PLAYER); // ???
+			ub.setUsername("Demo User"); // ???
+			ub.setAvater(";-)"); // ???
 			return ub;
+		} else {
+			throw new NameNotFoundException("Invalid user name/email");
 		}
+	}
+
+	// Sapir - User related API - Create a new user
+	@RequestMapping(path = "/acs/users", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public UserBoundary CreateNewUser(@RequestBody NewUserDetailsBoundary userDetails) {
+		UserBoundary ub = new UserBoundary();
+		ub.setUserId(new UserIdBoundary("2020b.demo", userDetails.getEmail())); // ???
+		ub.setTypeRole(userDetails.getTypeRole());
+		ub.setUsername(userDetails.getUsername());
+		ub.setAvater(userDetails.getAvater());
+		return ub;
+	}
 	
 }
