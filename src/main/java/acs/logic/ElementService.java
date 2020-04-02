ab38c068f551a46f17036ad2c80d129382bb47b3
yonat <yonat@DESKTOP-3R74DER>
2020-04-02 19:24:33 +0300

    elements interface update by yonatan

--- src/main/java/acs/logic/ElementService.java
+++ src/main/java/acs/logic/ElementService.java
@@ -1,5 +1,16 @@
 package acs.logic;
 
+import java.util.List;
+
+import acs.rest.boundaries.ElementBoundary;
+import acs.rest.boundaries.UserBoundary;
+
 public interface ElementService {
 
+	public ElementBoundary createUser();
+	public ElementBoundary update();
+	public List<ElementBoundary> getAllUsers();
+	public ElementBoundary getSpecificElements();
+	public void deleteAllActions();
+	
 }
