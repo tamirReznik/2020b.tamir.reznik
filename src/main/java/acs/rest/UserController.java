69c8412e4e44ce018e3240d4671fb5c750491006
sapir <sapir@LAPTOP-B36N6R6T>
2020-04-03 17:12:41 +0300

    by Sapir - change in method createUser

--- src/main/java/acs/rest/UserController.java
+++ src/main/java/acs/rest/UserController.java
@@ -1,6 +1,5 @@
 package acs.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,46 +8,31 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import org.springframework.web.bind.annotation.RestController;
 
-
-import acs.logic.UserService;
+import acs.NameNotFoundException;
+import acs.TypeEnumRole;
 import acs.rest.boundaries.NewUserDetailsBoundary;
 import acs.rest.boundaries.UserBoundary;
 import acs.rest.boundaries.UserIdBoundary;
 
 @RestController
 public class UserController {
-	private UserService userService;
-	
-	@Autowired
-	public UserController() {
-	}
-	
-	public UserController(UserService userService) {
-		super();
-		this.userService = userService;
-	}
-	@Autowired
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
 
 	// Sapir - User related API - Login valid user
 	@RequestMapping(path = "/acs/users/login/{userDomain}/{userEmail}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+
 	public UserBoundary loginValidUser(@PathVariable("userDomain") String userDomain,
 			@PathVariable("userEmail") String userEmail) {
-		return this.userService.login(userDomain, userEmail);
-//		return userService.login(userDomain, userEmail);
-//		if (userDomain != null && !userDomain.trim().isEmpty() && userEmail != null && !userEmail.trim().isEmpty()) {
-//			// return new UserBoundary();
-//			UserBoundary ub = new UserBoundary();
-//			ub.setUserId(new UserIdBoundary(userDomain, userEmail));
-//			ub.setRole(TypeEnumRole.PLAYER);
-//			ub.setUsername("Demo User");
-//			ub.setAvatar(";-)");
-//			return ub;
-//		} else {
-//			throw new NameNotFoundException("Invalid user name/email");
-//		}
+		if (userDomain != null && !userDomain.trim().isEmpty() && userEmail != null && !userEmail.trim().isEmpty()) {
+			// return new UserBoundary();
+			UserBoundary ub = new UserBoundary();
+			ub.setUserId(new UserIdBoundary(userDomain, userEmail));
+			ub.setRole(TypeEnumRole.PLAYER);
+			ub.setUsername("Demo User");
+			ub.setAvatar(";-)");
+			return ub;
+		} else {
+			throw new NameNotFoundException("Invalid user name/email");
+		}
 	}
 
 	// Sapir - User related API - Create a new user
@@ -60,13 +44,14 @@ public class UserController {
 		ub.setRole(userDetails.getRole());
 		ub.setUsername(userDetails.getUsername());
 		ub.setAvatar(userDetails.getAvatar());
-		return null;
+		return ub;
 	}
 
 	// Sapir - User related API - Update user details
 	@RequestMapping(path = "/acs/users/{userDomain}/{userEmail}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void updateUserDetails(@PathVariable("userDomain") String userDomain,
-			@PathVariable("userEmail") String userEmail, @RequestBody UserBoundary update) {
+			@PathVariable("userEmail") String userEmail,
+			@RequestBody UserBoundary update) {
 		// TODO emplement updateUserDetails method
 	}
 
