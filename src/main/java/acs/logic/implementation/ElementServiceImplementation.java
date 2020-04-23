ae76e036d283858c2fdc5f742e7dc0827e3f8039
yonat <yonat@DESKTOP-3R74DER>
2020-04-23 11:49:53 +0300

    update

--- src/main/java/acs/logic/implementation/ElementServiceImplementation.java
+++ src/main/java/acs/logic/implementation/ElementServiceImplementation.java
@@ -60,7 +60,7 @@ public class ElementServiceImplementation implements ElementService {
 	@Override
 	public ElementBoundary update(String managerDomain, String managerEmail, String elementDomain, String elementId,
 			ElementBoundary update) {
-		ElementEntity existing = this.elementDatabase.get(elementId);
+		ElementEntity existing = this.elementDatabase.get(elementDomain + "#" + elementId);
 		if (existing == null)
 			throw new ObjectNotFoundException("could not find object by id:" + elementId);
 		else {
@@ -90,7 +90,7 @@ public class ElementServiceImplementation implements ElementService {
 	@Override // check for null in arguments
 	public ElementBoundary getSpecificElement(String userDomain, String userEmail, String elementDomain,
 			String elementId) {
-		ElementEntity existing = this.elementDatabase.get(elementId);
+		ElementEntity existing = this.elementDatabase.get(elementDomain + "#" + elementId);
 		if (existing != null) {
 			return this.converter.fromEntity(existing);
 		} else {
