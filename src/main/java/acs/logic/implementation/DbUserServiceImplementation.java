80b459e23816fe58780fd13e25e1c1faaba1f6d2
Tamir Reznik <tamir4990@gmail.com>
2020-05-17 18:50:56 +0300

    use service tools

--- src/main/java/acs/logic/implementation/DbUserServiceImplementation.java
+++ src/main/java/acs/logic/implementation/DbUserServiceImplementation.java
@@ -12,6 +12,7 @@ import acs.data.Converter;
 import acs.data.UserEntity;
 import acs.data.UserIdEntity;
 import acs.logic.ObjectNotFoundException;
+import acs.logic.ServiceTools;
 import acs.logic.UserService;
 import acs.rest.boundaries.user.UserBoundary;
 import java.util.regex.Pattern;
@@ -46,12 +47,13 @@ public class DbUserServiceImplementation implements UserService {
 			throw new ObjectNotFoundException("Invalid email address");
 
 		// User name integrity check
-		if (user.getUsername() == null)
-			throw new ObjectNotFoundException("Username cannot be null");
+//		if (user.getUsername() == null)
+//			throw new ObjectNotFoundException("Username cannot be null");
 
 		// Avatar integrity check
-		if (user.getAvatar() == null || user.getAvatar().trim().isEmpty())
-			throw new ObjectNotFoundException("Avatar cannot be null or empty");
+//		if (user.getAvatar() == null || user.getAvatar().trim().isEmpty())
+//			throw new ObjectNotFoundException("Avatar cannot be null or empty");
+		ServiceTools.stringValidation(user.getAvatar(), user.getUsername());
 
 		user.getUserId().setDomain(projectName);
 
@@ -64,16 +66,16 @@ public class DbUserServiceImplementation implements UserService {
 	@Transactional(readOnly = true)
 	public UserBoundary login(String userDomain, String userEmail) {
 		// UserEntity existing = this.usersDatabase.get(userDomain + "#" + userEmail);
+		ServiceTools.stringValidation(userDomain, userEmail);
 
 		UserIdEntity userId = new UserIdEntity(userDomain, userEmail);
 		Optional<UserEntity> existing = this.userDao.findById(userId);
 
-		if (existing.isPresent()) {
+		if (existing.isPresent())
 			return this.converter.fromEntity(existing.get());
-		} else {
+		else
 			throw new ObjectNotFoundException(
 					"could not find object by UserDomain: " + userDomain + "or userEmail: " + userEmail);
-		}
 
 	}
 
@@ -81,62 +83,50 @@ public class DbUserServiceImplementation implements UserService {
 	@Transactional // (readOnly = false)
 	public UserBoundary updateUser(String userDomain, String userEmail, UserBoundary update) {
 
-		if (userDomain != null && !userDomain.trim().isEmpty() && userEmail != null && !userEmail.trim().isEmpty()) {
+		ServiceTools.stringValidation(userDomain, userEmail);
 
-			UserIdEntity userId = new UserIdEntity(userDomain, userEmail);
-			UserEntity existing = this.userDao.findById(userId).orElseThrow(() -> new ObjectNotFoundException(
-					"could not find object by UserDomain: " + userDomain + "or userEmail: " + userEmail));
+		UserIdEntity userId = new UserIdEntity(userDomain, userEmail);
+		UserEntity existing = this.userDao.findById(userId).orElseThrow(() -> new ObjectNotFoundException(
+				"could not find object by UserDomain: " + userDomain + "or userEmail: " + userEmail));
 
-			if (update.getRole() != null) {
-				existing.setRole(this.converter.toEntity(update.getRole()));
-			}
-
-			if (update.getUsername() != null) {
-				existing.setUsername(update.getUsername());
-			}
-
-			if (update.getAvatar() != null && !(update.getAvatar().trim().isEmpty())) {
-				existing.setAvatar(update.getAvatar());
-			}
-
-			// Data Access Object (DAO)
-			return this.converter.fromEntity(this.userDao.save(existing));
-
-		} else {
-			throw new RuntimeException("User Domain and User Email must not be empty or null");
-
+		if (update.getRole() != null) {
+			existing.setRole(this.converter.toEntity(update.getRole()));
 		}
+
+		if (update.getUsername() != null) {
+			existing.setUsername(update.getUsername());
+		}
+
+		if (update.getAvatar() != null && !(update.getAvatar().trim().isEmpty())) {
+			existing.setAvatar(update.getAvatar());
+		}
+
+		// Data Access Object (DAO)
+		return this.converter.fromEntity(this.userDao.save(existing));
+
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public List<UserBoundary> getAllUsers(String adminDomain, String adminEmail) {
-		if (adminDomain != null && !adminDomain.trim().isEmpty() && adminEmail != null
-				&& !adminEmail.trim().isEmpty()) {
 
-			Iterable<UserEntity> allUsers = this.userDao.findAll();
+		ServiceTools.stringValidation(adminDomain, adminEmail);
 
-			List<UserBoundary> returnUsers = new ArrayList<>();
-			for (UserEntity entity : allUsers) {
-				returnUsers.add(this.converter.fromEntity(entity)); // map entities to boundaries
-			}
+		Iterable<UserEntity> allUsers = this.userDao.findAll();
 
-			return returnUsers;
+		List<UserBoundary> returnUsers = new ArrayList<>();
+		for (UserEntity entity : allUsers)
+			returnUsers.add(this.converter.fromEntity(entity)); // map entities to boundaries
 
-		} else {
-			throw new RuntimeException("Admin Domain and Admin Email must not be empty or null");
+		return returnUsers;
 
-		}
 	}
 
 	@Override
 	@Transactional // (readOnly = false)
 	public void deleteAllUsers(String adminDomain, String adminEmail) {
-		if (adminDomain != null && !adminDomain.trim().isEmpty() && adminEmail != null
-				&& !adminEmail.trim().isEmpty()) {
-			this.userDao.deleteAll();
-		} else {
-			throw new RuntimeException("Admin Domain and Admin Email must not be empty or null");
-		}
+		ServiceTools.stringValidation(adminDomain, adminEmail);
+		this.userDao.deleteAll();
+
 	}
 }
